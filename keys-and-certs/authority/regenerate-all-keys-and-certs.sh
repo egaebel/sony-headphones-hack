9b69ad57ec870b0c9f7285886261f4e46b8aca5d
Ethan Gaebel <>
2021-04-08 00:05:05 -0700

    Refactoring to make mitm-functions general for all domains mitm-ed

--- keys-and-certs/authority/regenerate-all-keys-and-certs.sh
+++ keys-and-certs/authority/regenerate-all-keys-and-certs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# DNS names
+# DNS:bdcore-apr-lb.bda.ndmdhs.com,DNS:musiccenter-cdn.meta.ndmdhs.com,DNS:api.iac.meta.ndmdhs.com,DNS:mds.csx.sony.com,DNS:hc01.prc.sonydna.com,DNS:info.update.sony.net
+
 # Set up certificate authority.
 openssl genrsa -out ca.key 4096
 openssl req -new -x509 -days 1826 -key ca.key -out ca.crt
@@ -7,9 +10,9 @@ openssl req -new -x509 -days 1826 -key ca.key -out ca.crt
 # Set up server.
 openssl genrsa -out server.key 4096
 # openssl req -new -key server.key -out server.csr
-openssl req -new -addext "subjectAltName = DNS:musiccenter-cdn.meta.ndmdhs.com,DNS:bdcore-apr-lb.bda.ndmdhs.com" -key server.key -out server-2.csr
+openssl req -new -addext "subjectAltName = DNS:bdcore-apr-lb.bda.ndmdhs.com,DNS:musiccenter-cdn.meta.ndmdhs.com,DNS:api.iac.meta.ndmdhs.com,DNS:mds.csx.sony.com,DNS:hc01.prc.sonydna.com,DNS:info.update.sony.net" -key server.key -out server-2.csr
 
 
 # Get server certificate from certificate authority.
 # openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 9999 -sha256
-openssl x509 -req -extfile <(printf "subjectAltName = DNS:musiccenter-cdn.meta.ndmdhs.com,DNS:bdcore-apr-lb.bda.ndmdhs.com") -in server-2.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server-2.crt -days 9999 -sha256
+openssl x509 -req -extfile <(printf "subjectAltName = DNS:bdcore-apr-lb.bda.ndmdhs.com,DNS:musiccenter-cdn.meta.ndmdhs.com,DNS:api.iac.meta.ndmdhs.com,DNS:mds.csx.sony.com,DNS:hc01.prc.sonydna.com,DNS:info.update.sony.net") -in server-2.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server-2.crt -days 9999 -sha256
--- rust-mitm-server/src/main.rs
+++ rust-mitm-server/src/main.rs
@@ -1,15 +1,16 @@
 use actix_web::dev::HttpResponseBuilder;
-use actix_web::http::header::{HeaderMap, HeaderValue};
+use actix_web::http::header::HeaderValue;
 use actix_web::http::{Method, StatusCode};
-use actix_web::{get, middleware, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, middleware, web, App, FromRequest, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use reqwest::blocking::Client;
 use rustls::internal::pemfile::{certs, rsa_private_keys};
 use rustls::sign::{RSASigningKey, SigningKey};
 use rustls::{NoClientAuth, ResolvesServerCertUsingSNI, ServerConfig};
 
 use lazy_static::lazy_static;
-use std::collections::BTreeMap;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::fs::File;
 use std::io::BufReader;
 use std::sync::{Arc, Mutex};
@@ -17,6 +18,8 @@ use std::sync::{Arc, Mutex};
 const BDCORE_DOMAIN: &str = "bdcore-apr-lb.bda.ndmdhs.com";
 const BDCORE_URL: &str = "https://bdcore-apr-lb.bda.ndmdhs.com";
 const _CLOUDFRONT_URL: &str = "https://server-54-230-126-18.hio50.r.cloudfront.net";
+const INFO_UPDATE_SONY_DOMAIN: &str = "info.update.sony.net";
+const INFO_UPDATE_SONY_URL: &str = "https://info.update.sony.net";
 const MUSIC_CENTER_DOMAIN: &str = "musiccenter-cdn.meta.ndmdhs.com";
 const MUSIC_CENTER_URL: &str = "https://musiccenter-cdn.meta.ndmdhs.com";
 const SERVING_IP_ADDRESS: &str = "192.168.1.64";
@@ -28,6 +31,8 @@ struct CachedRequest {
     request_type: String,
     host: String,
     resource: String,
+    body: String,
+    // TODO: Should headers be added? They have a bunch of time elements.....
 }
 
 #[derive(Debug, Eq, Hash, PartialEq)]
@@ -37,11 +42,12 @@ struct CachedResponse {
 }
 
 impl CachedRequest {
-    fn new(request_type: &Method, host: &str, resource: &str) -> CachedRequest {
+    fn new(request_type: &Method, host: &str, resource: &str, body: &str) -> CachedRequest {
         return CachedRequest {
             request_type: String::from(request_type.as_str()),
             host: String::from(host),
             resource: String::from(resource),
+            body: String::from(body),
         };
     }
 }
@@ -119,14 +125,66 @@ fn convert_to_reqwest_headers(
     return header_map;
 }
 
-fn mitm(request_type: &Method, host: &str, resource: &str, req: &HttpRequest) -> HttpResponse {
+fn make_client_request(
+    request_type: &Method,
+    url: &str,
+    body: &str,
+    req: &HttpRequest,
+) -> reqwest::Result<reqwest::blocking::Response> {
+    println!("'{}' request against url: '{}'", request_type.as_str(), url);
     let client = Client::new();
+    return match request_type {
+        &Method::POST => client
+            .post(url)
+            .headers(convert_to_reqwest_headers(req.headers()))
+            .send(),
+        &Method::HEAD => client
+            .post(url)
+            .headers(convert_to_reqwest_headers(req.headers()))
+            .body(String::from(body))
+            .send(),
+        &Method::GET => client
+            .get(url)
+            .headers(convert_to_reqwest_headers(req.headers()))
+            .body(String::from(body))
+            .send(),
+        _ => panic!("Received unexpected request_type: '{:?}'", request_type),
+    };
+}
+
+fn get_request_string_url(request_type: &Method, url: &str) -> String {
+    return String::from(format!(
+        "method '{}' for endpoint: '{}'",
+        request_type.as_str(),
+        url
+    ));
+}
+
+fn get_request_string(request_type: &Method, host: &str, resource: &str) -> String {
+    return get_request_string_url(request_type, &format!("{}{}", host, resource));
+}
+
+fn mitm(
+    request_type: &Method,
+    host: &str,
+    resource: &str,
+    request_body: &str,
+    req: &HttpRequest,
+) -> HttpResponse {
+    let caching_domains: Vec<&str> = vec![MUSIC_CENTER_DOMAIN];
+
+    // Option to just return an empty OK response.
     const RETURN_EMPTY_OKAY: bool = false;
     if RETURN_EMPTY_OKAY {
         return HttpResponse::new(StatusCode::OK);
     }
-    let cached_request = CachedRequest::new(request_type, host, resource);
-    if USE_RESPONSE_CACHE && CACHE.lock().unwrap().contains_key(&cached_request) {
+
+    // If caching is enabled check the cache and return the response.
+    let cached_request = CachedRequest::new(request_type, host, resource, request_body);
+    if USE_RESPONSE_CACHE
+        && caching_domains.contains(&host)
+        && CACHE.lock().unwrap().contains_key(&cached_request)
+    {
         println!(
             "Returning response from cache for request: '{:?}'.....",
             cached_request
@@ -138,73 +196,54 @@ fn mitm(request_type: &Method, host: &str, resource: &str, req: &HttpRequest) ->
             .unwrap()
             .to_http_response();
     }
+    let url: String;
     if host == MUSIC_CENTER_DOMAIN {
         println!("Serving music center domain.....");
-
-        let url = format!("{}{}", MUSIC_CENTER_URL, resource);
-        match client
-            .head(url)
-            .headers(convert_to_reqwest_headers(req.headers()))
-            .send()
-        {
-            Ok(resp) => {
-                println!("Response: '{:?}'", resp);
-                let mut resp_builder = HttpResponse::Ok();
-                set_headers_in_resp(resp.headers(), &mut resp_builder);
-                let string_headers = convert_to_string_headers(resp.headers());
-                let text = resp.text().unwrap();
-                println!("Response.text(): '{:?}'", text);
-                if USE_RESPONSE_CACHE {
-                    println!("Inserting request/response pair into cache.....");
-                    CACHE.lock().unwrap().insert(
-                        CachedRequest::new(request_type, host, resource),
-                        CachedResponse::new(string_headers, text),
-                    );
-                }
-                return resp_builder.finish();
-            }
-            Err(e) => panic!("Error: {:?}", e),
-        }
+        url = format!("{}{}", MUSIC_CENTER_URL, resource);
     } else if host == BDCORE_DOMAIN {
         println!("Serving bdcore domain.....");
-        let url = format!("{}{}", BDCORE_URL, resource);
-        match client
-            .post(url)
-            .headers(convert_to_reqwest_headers(req.headers()))
-            .send()
-        {
-            Ok(resp) => {
-                println!("Response: '{:?})'", resp);
-                let mut resp_builder = HttpResponse::Ok();
-                let mut string_headers: HashMap<String, String> = HashMap::new();
-                for (key, value) in resp.headers() {
-                    string_headers.insert(
-                        String::from(key.as_str()),
-                        String::from(value.to_str().unwrap()),
-                    );
-                    resp_builder.set_header(
-                        key.as_str(),
-                        HeaderValue::from_str(value.to_str().unwrap()).unwrap(),
-                    );
-                }
-                let text = resp.text().unwrap();
-                println!("Response.text(): '{:?}'", text);
-                if USE_RESPONSE_CACHE {
-                    println!("Inserting request/response pair into cache.....");
-                    CACHE.lock().unwrap().insert(
-                        CachedRequest::new(request_type, host, resource),
-                        CachedResponse::new(string_headers, text),
-                    );
-                }
-                return resp_builder.finish();
-            }
-            Err(e) => panic!("Error: {:?}", e),
-        }
+        url = format!("{}{}", BDCORE_URL, resource);
+    } else if host == INFO_UPDATE_SONY_DOMAIN {
+        println!("Serving info update sony domain.....");
+        url = format!("{}{}", INFO_UPDATE_SONY_URL, resource);
+    } else {
+        panic!("Man-in-the-middle services are not available for request to host '{}'. Please try again :D.", host);
     }
-    println!("Man-in-the-middle services are not avaialbe for request to host '{}'. Please try again :D.", host);
-    return HttpResponse::Ok().body(String::from(
-        "Hello, man-in-the-middle services are not avaialbe for this request. Please try again :D.",
-    ));
+    match make_client_request(request_type, &url, request_body, req) {
+        Ok(resp) => {
+            println!(
+                "Response from {}: '{:?}'",
+                get_request_string(request_type, host, resource),
+                resp
+            );
+            let resp_status_code = resp.status();
+            let mut resp_builder = HttpResponse::Ok();
+            set_headers_in_resp(resp.headers(), &mut resp_builder);
+            let string_headers = convert_to_string_headers(resp.headers());
+            let text = resp.text().unwrap();
+            println!(
+                "Response.text() from {}: '{:?}'",
+                get_request_string(request_type, host, resource),
+                text
+            );
+            if USE_RESPONSE_CACHE
+                && caching_domains.contains(&host)
+                && resp_status_code == StatusCode::OK
+            {
+                println!("Inserting request/response pair into cache.....");
+                CACHE.lock().unwrap().insert(
+                    CachedRequest::new(request_type, host, resource, request_body),
+                    CachedResponse::new(string_headers, text),
+                );
+            }
+            return resp_builder.finish();
+        }
+        Err(e) => panic!(
+            "Error serving {}: {:?}",
+            get_request_string_url(request_type, &url),
+            e
+        ),
+    }
 }
 
 #[actix_web::main]
@@ -251,11 +290,17 @@ async fn main() -> std::io::Result<()> {
             // enable logger
             .wrap(middleware::Logger::default())
             // .service(index)
-            .default_service(web::to(|request: HttpRequest| {
-                println!("\n====================================");
-                println!("Request: '{:?}'", request);
-                println!("Request.head(): '{:?}'", request.head());
-                println!("Request.uri(): '{:?}'", request.uri());
+            .app_data(String::configure(|cfg| {  // <- limit size of the payload
+                cfg.limit(2 << 16)
+            }))
+            .default_service(web::to(|body: String, request: HttpRequest| {
+                println!(
+                    "\n====================================\nRequest: '{:?}'\nRequest.head(): '{:?}'\nRequest.uri(): '{:?}'\nRequest.body(): '{:?}'",
+                    request,
+                    request.head(),
+                    request.uri(),
+                    body,
+                );
                 let uri = request.uri();
                 let host = match request.headers().contains_key("host") {
                     true => request.headers().get("host").unwrap().to_str().unwrap(),
@@ -264,13 +309,18 @@ async fn main() -> std::io::Result<()> {
                 let path = &request.uri().path();
                 let method = request.method();
                 println!(
-                    "Default service hit with method '{}' for endpoint: {}{}",
-                    method.as_str(),
+                    "Default service hit with {}",
+                    get_request_string(
+                    method,
                     host,
-                    path
+                    path)
                 );
-                let response = mitm(method, host, path, &request);
-                println!("Response going back from mitm server: '{:?}'", response);
+                let response = mitm(method, host, path, &body, &request);
+                println!("Response going back from mitm server for {}: '{:?}'",
+                get_request_string(
+                    method,
+                    host,
+                    path), response);
                 return response;
             }))
     })
